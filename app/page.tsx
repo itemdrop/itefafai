e8d08562d1c53283ffc48717a2d989f83ad78315
Fasolia101 <faisalfaisal0797@gmail.com>
2025-10-07 16:27:55 +0000

    Committing all changes

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            faisal by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Learn more about Next.js.
+            Learn more about FAISALS MOM Next.js.s
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
